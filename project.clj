1a2bea35d8a18ae3ff709d27776c80e8ed766115
Greg Sexton <gregsexton@gmail.com>
2013-06-16 21:37:31 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clout "0.1.0-SNAPSHOT"
+  :description "A Clojure port of libshout"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ test/clout/core_test.clj
@@ -0,0 +1,7 @@
+(ns clout.core-test
+  (:use clojure.test
+        clout.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
