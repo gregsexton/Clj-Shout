9a7321dcd44ce33728c9067fdf4f827139c849d4
Greg Sexton <gregsexton@gmail.com>
2013-07-10 21:29:07 +0100

    Create builder

--- src/clout/core.clj
+++ src/clout/core.clj
@@ -1,6 +1,29 @@
 (ns clout.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def default-host "localhost")
+(def default-port 8000)
+(def default-format :mp3)
+(def default-protocol :http)
+(def default-user "source")
+(def default-useragent "clout")
+
+(defmacro defvalue [name key default]
+  `(defn ~name [val#]
+     (if val# [~key val#] [~key ~default])))
+(defvalue with-host :hostname default-host)
+(defvalue with-port :port default-port)
+(defvalue with-format :format default-format)
+(defvalue with-protocol :protocol default-protocol)
+(defvalue with-user :user default-user)
+(defvalue with-agent :agent default-useragent)
+(defvalue with-password :password
+  (throw (IllegalArgumentException. "No password provided")))
+
+(defn create-clout-session [& pairs]
+  (let [default-map (into {} (list (with-host nil)
+                                   (with-port nil)
+                                   (with-format nil)
+                                   (with-protocol nil)
+                                   (with-user nil)
+                                   (with-agent nil)))]
+    (reduce conj default-map pairs)))
--- test/clout/core_test.clj
+++ test/clout/core_test.clj
@@ -1,7 +1,46 @@
 (ns clout.core-test
-  (:use clojure.test
+  (:use midje.sweet
         clout.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "about creating a new session"
+       (fact "should provide all defaults"
+             (create-clout-session) => (contains
+                                        {:hostname "localhost"
+                                         :port 8000
+                                         :format :mp3
+                                         :protocol :http
+                                         :user "source"
+                                         :agent "clout"}))
+
+       (fact "should provide defaults for missing builder values"
+              (create-clout-session
+               (with-port 1234)) => (contains
+                                     {:hostname "localhost"
+                                      :port 1234
+                                      :format :mp3
+                                      :protocol :http
+                                      :user "source"
+                                      :agent "clout"}))
+
+       (fact "should provide a readable builder syntax for setting every possible option at once"
+             (create-clout-session
+              (with-host "host")
+              (with-port 1234)
+              (with-format :mp3)
+              (with-protocol :http)
+              (with-user "foo")
+              (with-password "hackme")
+              (with-agent "agent")) => (contains
+                                        {:hostname "host"
+                                         :port 1234
+                                         :format :mp3
+                                         :protocol :http
+                                         :user "foo"
+                                         :agent "agent"
+                                         :password "hackme"})))
+
+
+
+(facts "about connecting"
+       (fact "should connect without error"
+             ))
