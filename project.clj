5a1a9c2ff5bd7953797e0ab161e42121f7202e7f
Greg Sexton <gregsexton@gmail.com>
2013-07-10 21:29:01 +0100

    Setup project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clout "0.1.0-SNAPSHOT"
   :description "A Clojure port of libshout"
-  :url ""
+  :url "http://www.gregsexton.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.1"]]
+  :plugins [[lein-midje "3.0.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
