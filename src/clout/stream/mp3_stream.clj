b206201afe07c850f44304af2b53fb8ab12c3e34
Greg Sexton <gregsexton@gmail.com>
2013-08-06 21:04:27 +0100

    Implement most of mp3 header parsing

--- src/clout/stream/mp3_stream.clj
+++ src/clout/stream/mp3_stream.clj
@@ -1,7 +1,91 @@
 (ns clout.stream.mp3-stream
   (:require [clout.stream.stream :refer [OutStream]]))
 
-(deftype Mp3OutStream [stream]
+(defn bit-extract
+  "Extract certain bits from an integer returning them as an
+  integer. start is how many bits from the right to look at, length is
+  the number of bits from this position to extract."
+  [x start length]
+  (let [mask (reduce #(bit-or %2 (bit-shift-left %1 1))
+                     0 (repeat length 1))]
+    (bit-and (bit-shift-right x (- start length)) mask)))
+
+(defn combine-bytes [b1 b2 b3 b4]
+  (unchecked-int
+   (bit-or (bit-shift-left b1 24)
+           (bit-shift-left b2 16)
+           (bit-shift-left b3 8)
+           b4)))
+
+(defn lookup-version [raw]
+  (condp = raw
+    0 25
+    1 (throw (IllegalArgumentException. "Invalid version: reserved"))
+    2 2
+    3 1
+    (throw
+     (IllegalArgumentException. "Invalid version: exceeds two bits"))))
+
+(defn lookup-layer [raw]
+  (condp = raw
+    0 (throw (IllegalArgumentException. "Invalid layer: reserved"))
+    1 3
+    2 2
+    3 1
+    (throw
+     (IllegalArgumentException. "Invalid layer: exceeds two bits"))))
+
+(defn lookup-bitrate [version layer index]
+  {:pre [(> index 0) (< index 15)]}
+  (let [lookup {1   {:v1 {:l1 32  :l2 32  :l3 32}  :v2 {:l1 32  :l2 8   :l3 8}}
+                2   {:v1 {:l1 64  :l2 48  :l3 40}  :v2 {:l1 48  :l2 16  :l3 16}}
+                3   {:v1 {:l1 96  :l2 56  :l3 48}  :v2 {:l1 56  :l2 24  :l3 24}}
+                4   {:v1 {:l1 128 :l2 64  :l3 56}  :v2 {:l1 64  :l2 32  :l3 32}}
+                5   {:v1 {:l1 160 :l2 80  :l3 64}  :v2 {:l1 80  :l2 40  :l3 40}}
+                6   {:v1 {:l1 192 :l2 96  :l3 80}  :v2 {:l1 96  :l2 48  :l3 48}}
+                7   {:v1 {:l1 224 :l2 112 :l3 96}  :v2 {:l1 112 :l2 56  :l3 56}}
+                8   {:v1 {:l1 256 :l2 128 :l3 112} :v2 {:l1 128 :l2 64  :l3 64}}
+                9   {:v1 {:l1 288 :l2 160 :l3 128} :v2 {:l1 144 :l2 80  :l3 80}}
+                10  {:v1 {:l1 320 :l2 192 :l3 160} :v2 {:l1 160 :l2 96  :l3 96}}
+                11  {:v1 {:l1 352 :l2 224 :l3 192} :v2 {:l1 176 :l2 112 :l3 112}}
+                12  {:v1 {:l1 384 :l2 256 :l3 224} :v2 {:l1 192 :l2 128 :l3 128}}
+                13  {:v1 {:l1 416 :l2 320 :l3 256} :v2 {:l1 224 :l2 144 :l3 144}}
+                14  {:v1 {:l1 448 :l2 384 :l3 320} :v2 {:l1 256 :l2 160 :l3 160}}}
+        v (condp = version
+            1 :v1 2 :v2 25 :v2
+            (throw (IllegalArgumentException.
+                    (format "Unexpected version: " version))))
+        l (condp = layer
+            1 :l1 2 :l2 3 :l3
+            (throw (IllegalArgumentException.
+                    (format "Unexpected layer: " layer))))]
+    (->> index
+         (get lookup)
+         v
+         l)))
+
+(defn lookup-samplerate [version index]
+  44100)                                ;todo
+
+(defn maybe-parse-header [[b1 b2 b3 b4]]
+  (let [head (combine-bytes b1 b2 b3 b4)
+        sync (bit-extract head 32 11)
+        version (lookup-version (bit-extract head 21 2))
+        layer (lookup-layer (bit-extract head 19 2))]
+    {:valid-sync? (= sync 2047)         ;11 bits set
+     :version version
+     :layer layer
+     :error-protection? (= (bit-extract head 17 1) 0)
+     :bitrate (lookup-bitrate version layer (bit-extract head 16 4))
+     :samplerate (lookup-samplerate version (bit-extract head 12 2))
+     :padded? (= (bit-extract head 10 1) 1)
+     :stereo? (not= (bit-extract head 8 2) 3)
+     :mode-ext-code (bit-extract head 6 2)  ;raw
+     :copyright? (= (bit-extract head 4 1) 1)
+     :original? (= (bit-extract head 4 1) 0)
+     :emphasis? (not= (bit-extract head 4 1) 0)}))
+
+(deftype SynchronousMp3OutStream [stream]
   OutStream
 
   (write [this bytes])
---
+++ test/clout/stream/mp3_stream_test.clj
@@ -0,0 +1,85 @@
+(ns clout.stream.mp3-stream-test
+  (:use midje.sweet
+        clout.stream.mp3-stream))
+
+(facts "about bit-extraction"
+       (fact "should deal with the low end"
+             (bit-extract 1 1 1) => 1
+             (bit-extract 10 4 4) => 10
+             (bit-extract 10 4 1) => 1
+             (bit-extract 10 3 1) => 0
+             (bit-extract 10 2 1) => 1
+             (bit-extract 10 1 1) => 0)
+       (fact "should deal with the high end"
+             (bit-extract 0xaaaaaaaa 32 8) => 0xaa
+             (bit-extract 0xaaaaaaaa 32 1) => 1
+             (bit-extract 0xfffba040 20 1) => 1
+             (bit-extract 0xfffba040 19 2) => 1
+
+             (bit-extract 0xfffba040 32 8) => 0xff
+             (bit-extract 0xfffba040 24 8) => 0xfb
+             (bit-extract 0xfffba040 16 8) => 0xa0
+             (bit-extract 0xfffba040 8 8)  => 0x40))
+
+(facts "about combining bytes"
+       (fact "should combine correctly"
+             (combine-bytes (unchecked-int 0xff)
+                            (unchecked-int 0xfb)
+                            (unchecked-int 0xa0)
+                            (unchecked-int 0x40)) => (unchecked-int 0xfffba040)))
+
+(facts "about attempting to parse a header"
+
+       ;; http://mpgedit.org/mpgedit/mpeg_format/MP3Format.html
+
+       (future-fact "should return nil given an invalid header"
+                    ;; todo: perform checks as in mp3_header
+                    (maybe-parse-header [5 6 7 8]) => nil)
+
+       (let [header [(unchecked-int 0xff)
+                     (unchecked-int 0xfb)
+                     (unchecked-int 0xa0)
+                     (unchecked-int 0x40)]]
+         (fact "should successfully parse a full example header"
+               (maybe-parse-header header) => (contains {:valid-sync? true})
+               (maybe-parse-header header) => (contains {:version 1})
+               (maybe-parse-header header) => (contains {:layer 3})
+               (maybe-parse-header header) => (contains {:error-protection? false})
+               (maybe-parse-header header) => (contains {:bitrate 160})
+               (maybe-parse-header header) => (contains {:samplerate 44100})
+               (maybe-parse-header header) => (contains {:padded? false})
+               (maybe-parse-header header) => (contains {:stereo? true})
+               (maybe-parse-header header) => (contains {:copyright? false})
+               (maybe-parse-header header) => (contains {:original? true})
+               (maybe-parse-header header) => (contains {:emphasis? false})))
+
+       (fact "should lookup the version"
+             (maybe-parse-header [(unchecked-int 0xff)
+                                  (unchecked-int 0x07)
+                                  (unchecked-int 0xc0)
+                                  (unchecked-int 0x00)]) => (contains {:version 25}))
+
+       (fact "should lookup the layer"
+             (maybe-parse-header [(unchecked-int 0xff)
+                                  (unchecked-int 0x07)
+                                  (unchecked-int 0xc0)
+                                  (unchecked-int 0x00)]) => (contains {:layer 1}))
+
+       (fact "should lookup the bitrate correctly"
+             (lookup-bitrate 1 2 7) => 112
+             (lookup-bitrate 25 1 7) => 112
+             (lookup-bitrate 1 3 8) => 112
+             (lookup-bitrate 2 2 11) => 112
+             ;; version out of range
+             (lookup-bitrate 4 2 1) => (throws IllegalArgumentException)
+             ;; layer out of range
+             (lookup-bitrate 1 4 1) => (throws IllegalArgumentException)
+             (lookup-bitrate 1 2 0) => (throws AssertionError)
+             (lookup-bitrate 1 2 15) => (throws AssertionError)
+             (lookup-bitrate 1 2 16) => (throws AssertionError))
+
+       (future-fact "should lookup the samplesize correctly"))
+
+(facts "about calculating frame size"
+       (future-fact "should calculate frame-size"
+             (frame-size (maybe-parse-header header-bytes)) => ???))
