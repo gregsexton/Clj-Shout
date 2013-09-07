05dc832cc332b47539790cf4580182c2f2fcf708
Greg Sexton <gregsexton@gmail.com>
2013-09-07 17:19:38 +0100

    Remove intro.md

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shout "0.1.0-SNAPSHOT"
+(defproject shout "0.1.0"
   :description "A Clojure port of libshout"
   :url "http://www.gregsexton.org"
   :license {:name "Eclipse Public License"
