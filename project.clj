df5c7d7b3d527fd09c1d5e4c1ed3d5882e7fee0b
Greg Sexton <gregsexton@gmail.com>
2013-09-08 16:02:40 +0100

    Add github url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shout "0.1.0"
   :description "A Clojure port of libshout"
-  :url "http://www.gregsexton.org"
+  :url "https://github.com/gregsexton/Clj-Shout"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
