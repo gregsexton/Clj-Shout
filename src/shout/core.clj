a6d8ad7403b3c6b60b03355d693983f7ee0e226d
Greg Sexton <gregsexton@gmail.com>
2013-10-24 20:58:35 +0100

    Implement synchronous playlist

--- src/shout/core.clj
+++ src/shout/core.clj
@@ -35,7 +35,46 @@
 (defn send-source
   "Send a single source to the server as specified by session. Session
   should be created using shout.session/create-shout-session. The
-  source may be anything that clojure.java.io/input-stream can take."
+  source may be anything that clojure.java.io/input-stream can
+  take. This sends the source in realtime, i.e. it will take as long
+  as the song lasts to send it. This method is synchronous."
   [session source]
   (with-open [stream (create-out-stream session)]
     (stream/write stream (byte-seq source))))
+
+(defn create-playlist
+  "Create a playlist. Sources should be a seq of source maps. A source
+  map is defined as {:source source :metadata metadata}, where source
+  is anything that clojure.java.io/input-stream can take and metadata
+  is anything useful that you wish to attach to this item in the
+  playlist. The metadata is meant to be useful to the client and will
+  be ignored by Shout."
+  ([] (create-playlist []))
+  ([sources]
+     {:pre [(sequential? sources) (every? #(contains? % :source) sources)]}
+     (atom (seq sources))))
+
+(defn create-context
+  "Create a context. Contexts are used to send multiple sources as a
+  playlist to a session and maintain control over sending."
+  [playlist session]
+  {:playlist playlist
+   :session session
+   :current-source (atom 0)})
+
+(defn send-context
+  "Idempotently begin sending a context's data to the context's
+  session. This will happen asynchronously and return a new context."
+  [context]
+  (with-open [stream (create-out-stream (:session context))]
+    (doseq [source (->> context :playlist deref (map :source))]
+      (stream/write stream (byte-seq source)))))
+
+(def resume-context
+  "Idempotently resume sending the context's data to the context's
+  session. This will happen asynchronously and return a new context."
+  send-context)
+
+(defn append-to-playlist
+  "Append source to playlist."
+  [playlist source])
