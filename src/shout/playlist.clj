1327b805e603ab183850e96fc6cbbbd191c19273
Greg Sexton <gregsexton@gmail.com>
2013-12-21 14:31:36 +0000

    Add mutation operations to playlists
    As part of this, ditched the thread-safe mutable list
    implementation. Instead, use locking around an array list. This is just
    easier and the monitor overhead should be fine for this application.

--- src/shout/playlist.clj
+++ src/shout/playlist.clj
@@ -2,51 +2,69 @@
 
 ;;; Playlists must be mutable as they are closed over and read on a
 ;;; separate thread. Stopping and starting threads will disconnect
-;;; clients, so changes in state must be done on the fly.
-
-;;; This is implemented as cons cells wrapped in refs for coordination
-;;; purposes. Ref dereferencing is handled transparently. Consing (the
-;;; result of conj) mutably appends (O(n)). If this turns out to be
-;;; too inefficient maybe a rope would make a better datastructure?
-;;; This is purposely not chunked so as not to create extra
-;;; input-streams unnecessarily.
-
-(declare ->MutableList)
-
-(defn- empty-mutable-list []
-  (->MutableList (ref nil)))
-
-(deftype MutableList [head]
-  clojure.lang.Sequential
-  clojure.lang.ISeq
-  (first [_] (first (deref head)))
-  (next [_] (next (deref head)))
-  (more [_] (rest (deref head)))
-  (cons [this x]
-    (letfn [(last-ref-cons [coll]
-              (if (seq coll)
-                (recur (rest coll))
-                (.head coll)))]
-      (dosync
-       (let [last-ref-cons (last-ref-cons this)]
-         (ref-set last-ref-cons (MutableList. (ref (cons x (empty this)))))
-         this))))
-  (count [_] (count (deref head)))
-  (empty [_] (empty-mutable-list))
-  (equiv [this x] (identical? this x))
-  (seq [this] (when (deref head) this)))
+;;; clients, so changes in state must be done mutably.
 
 (defn create-playlist
-  "Create a playlist. Sources should be a seq of anything that
-  clojure.java.io/input-stream can take."
+  "Create a mutable thread-safe (can be modified concurrently and
+  should always be consistent) playlist. Sources should be a seq of
+  anything that clojure.java.io/input-stream can take."
   ([] (create-playlist []))
   ([sources]
      {:pre [(sequential? sources)]}
-     (reduce conj (empty-mutable-list) sources)))
+     (reduce #(when (.add %1 %2) %1)
+             (java.util.ArrayList.) sources)))
+
+(defmacro with-playlist
+  "Anaphoric macro (binds playlist to it) that ensures locking on the
+  playlist."
+  [playlist & body]
+  `(locking ~playlist
+     (let [~'it ~playlist]
+       ~@body)))
 
 (defn append-source!
   "Append a source to a playlist. A source is anything that
-  clojure.java.io/input-stream can take."
+  clojure.java.io/input-stream can take. Returns the modified
+  playlist."
   [playlist source]
-  {:pre [(instance? MutableList playlist)]}
-  (conj playlist source))
+  (with-playlist playlist
+    (when (.add it source) it)))
+
+(defn insert-source!
+  "Insert a source in to a playlist. A source is anything that
+  clojure.java.io/input-stream can take. If idx is greater than the
+  length of the playlist, the source will be appended. Returns the
+  modified playlist."
+  [playlist source idx]
+  {:pre [(>= idx 0)]}
+  (with-playlist playlist
+    (if (> idx (count it))
+      (append-source! it source)
+      (do (.add it idx source) it))))
+
+(defn remove-source!
+  "Remove a source from the playlist. This is a noop if idx isn't in
+  the playlist. Returns the modified playlist."
+  [playlist idx]
+  {:pre [(>= idx 0)]}
+  (with-playlist playlist
+    (when (< idx (count it))
+      (.remove #^java.util.List it (int idx)))
+    it))
+
+(defn move-source!
+  "Move a source in the playlist from one position to another. This is
+  a noop if the from-idx is outside the range of the playlist. The
+  source will be moved to the end of the playlist if to-idx is greater
+  than the length of the list. to-idx will be calculated as the index
+  before the source is moved. This operation is atomic."
+  [playlist from-idx to-idx]
+  {:pre [(>= from-idx 0) (>= to-idx 0)]}
+  (with-playlist playlist
+    (if (>= from-idx (count it))
+      it
+      (let [source (nth it from-idx)]
+        (insert-source! it source to-idx)
+        (remove-source! it (if (< to-idx from-idx)
+                                   (inc from-idx)
+                                   from-idx))))))
--- test/shout/playlist_test.clj
+++ test/shout/playlist_test.clj
@@ -2,38 +2,49 @@
   (:use midje.sweet
         shout.playlist))
 
-(facts "about mutable playlists"
-       (let [emp (->MutableList (ref nil))]
-         (fact "should work on an empty colletcion"
-               (first emp) => nil
-               (next emp) => nil
-               (rest emp) => '()
-               (count emp) => 0
-               (empty? emp) => true
-               (= emp emp) => true
-               (= emp (->MutableList (ref nil))) => false
-               (seq emp) => nil))
-       (let [one (conj (->MutableList (ref nil)) 1)]
-         (fact "should work on a collection of one value"
-               (first one) => 1
-               (next one) => nil
-               (rest one) => '()
-               (count one) => 1
-               (empty? one) => false
-               (= one one) => true
-               (seq one) => one))
-       (let [two (conj
-                  (conj (->MutableList (ref nil)) 1)
-                  2)]
-         (fact "should work on a collection of multiple values"
-               (first two) => 1
-               (next two) => '(2)
-               (rest two) => '(2)
-               (count two) => 2
-               (empty? two) => false
-               (= two two) => true
-               (seq two) => two)
-         (fact "should be mutable"
-               (identical? (conj two 3) two) => truthy)
-         (fact "should not be chunked"
-               (chunked-seq? two) => falsey)))
+(facts "about playlists"
+       (fact "should be able to create a playlist"
+             (create-playlist) => []
+             (create-playlist ["foo"]) => ["foo"]
+             (create-playlist ["foo1" "foo2"]) => ["foo1" "foo2"])
+
+       (fact "should be able to mutably append to a playlist"
+             (append-source! (create-playlist) "foo") => ["foo"]
+             (append-source! (create-playlist ["foo1"]) "foo2") => ["foo1" "foo2"]
+             (append-source! (create-playlist ["foo1" "foo2"]) "foo3") => ["foo1" "foo2" "foo3"])
+
+       (fact "should be able to mutably insert a source"
+             (insert-source! (create-playlist) "foo" 0) => ["foo"]
+             (insert-source! (create-playlist) "foo" 1) => ["foo"]
+             (insert-source! (create-playlist) "foo" -1) => (throws AssertionError)
+             (insert-source! (create-playlist ["foo"]) "bar" 0) => ["bar" "foo"]
+             (insert-source! (create-playlist ["foo"]) "bar" 1) => ["foo" "bar"]
+             (insert-source! (create-playlist ["foo" "baz"]) "bar" 0) => ["bar" "foo" "baz"]
+             (insert-source! (create-playlist ["foo" "baz"]) "bar" 1) => ["foo" "bar" "baz"]
+             (insert-source! (create-playlist ["foo" "baz"]) "bar" 2) => ["foo" "baz" "bar"])
+
+       (fact "should be able to mutably remove a source"
+             (remove-source! (create-playlist) 0) => []
+             (remove-source! (create-playlist) 1) => []
+             (remove-source! (create-playlist) -1) => (throws AssertionError)
+             (remove-source! (create-playlist ["foo"]) 0) => []
+             (remove-source! (create-playlist ["foo"]) 1) => ["foo"]
+             (remove-source! (create-playlist ["foo" "bar"]) 0) => ["bar"]
+             (remove-source! (create-playlist ["foo" "bar"]) 1) => ["foo"]
+             (remove-source! (create-playlist ["foo" "bar"]) 2) => ["foo" "bar"])
+
+       (fact "should be able to move a source"
+             (move-source! (create-playlist) 0 0) => []
+             (move-source! (create-playlist) 1 0) => []
+             (move-source! (create-playlist) 0 1) => []
+             (move-source! (create-playlist) 1 1) => []
+             (move-source! (create-playlist) -1 1) => (throws AssertionError)
+             (move-source! (create-playlist) 1 -1) => (throws AssertionError)
+             (move-source! (create-playlist ["foo" "bar"]) 0 0) => ["foo" "bar"]
+             (move-source! (create-playlist ["foo" "bar"]) 0 1) => ["foo" "bar"]
+             (move-source! (create-playlist ["foo" "bar"]) 0 2) => ["bar" "foo"]
+             (move-source! (create-playlist ["foo" "bar"]) 1 0) => ["bar" "foo"]
+             (move-source! (create-playlist ["foo" "bar"]) 1 1) => ["foo" "bar"]
+             (move-source! (create-playlist ["foo" "bar"]) 1 2) => ["foo" "bar"]
+             (move-source! (create-playlist ["foo" "bar"]) 2 1) => ["foo" "bar"]
+             (move-source! (create-playlist ["foo" "bar"]) 0 7) => ["bar" "foo"]))
