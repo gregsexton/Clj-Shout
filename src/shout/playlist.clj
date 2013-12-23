7220930261d312cd2208fd8e785aeaa58f963664
Greg Sexton <gregsexton@gmail.com>
2013-12-23 13:28:30 +0000

    Implement ConcurrentArrayList
    This decorates an ArrayList changing the iterator behaviour. This allows
    for concurrent modifications to the list while a thread is
    iterating. The iteration will see these updates. This makes use of
    locking and should be consistent.

--- src/shout/playlist.clj
+++ src/shout/playlist.clj
@@ -4,6 +4,61 @@
 ;;; separate thread. Stopping and starting threads will disconnect
 ;;; clients, so changes in state must be done mutably.
 
+;;; The playlist will be iterated over while concurrently
+;;; modified. The iterator should see these changes in a strongly
+;;; consistent manner. This implementation should provide this
+;;; behaviour.
+
+(deftype ConcurrentArrayList [^java.util.List list]
+  java.util.RandomAccess
+  java.util.List
+  (size [_] (locking list (.size list)))
+  (add [_ x] (locking list (.add list x)))
+  (add [_ idx x] (locking list (.add list idx x)))
+  (remove [_ ^int idx] (locking list (.remove list idx)))
+  (get [_ idx] (locking list (.get list idx)))
+  (iterator [_]
+    (letfn [(initial [cursor]
+              (fn [op]
+                (condp = op
+                  :has-next (if (< cursor (.size list))
+                              {:transition (cached cursor (.get list cursor))
+                               :has-next true}
+                              {:transition (ended)
+                               :has-next false})
+                  :next (if (< cursor (.size list))
+                          {:transition (initial (inc cursor))
+                           :next (.get list cursor)}
+                          {:transition (ended)
+                           :next (throw (java.util.NoSuchElementException.))}))))
+            (cached [cursor val]
+              (fn [op]
+                (condp = op
+                  :has-next {:transition (cached cursor val)
+                             :has-next true}
+                  :next {:transition (initial (inc cursor))
+                         :next val})))
+            (ended []
+              (fn [_] {:transition (ended)
+                       :has-next false
+                       :next (throw (java.util.NoSuchElementException.))}))
+            (transition [state op]
+              ((:transition state) op))]
+      (let [state (atom {:transition (initial 0)})]
+        (reify
+          java.util.Iterator
+          (hasNext [_]
+            (locking list
+              (swap! state transition :has-next)
+              (:has-next @state)))
+          (next [_]
+            (locking list
+              (swap! state transition :next)
+              (:next @state))))))))
+
+(defn ->ConcurrentArrayList []
+  (new ConcurrentArrayList (java.util.ArrayList.)))
+
 (defn create-playlist
   "Create a mutable thread-safe (can be modified concurrently and
   should always be consistent) playlist. Sources should be a seq of
@@ -11,46 +66,38 @@
   ([] (create-playlist []))
   ([sources]
      {:pre [(sequential? sources)]}
-     (reduce #(when (.add %1 %2) %1)
-             (java.util.ArrayList.) sources)))
-
-(defmacro with-playlist
-  "Anaphoric macro (binds playlist to it) that ensures locking on the
-  playlist."
-  [playlist & body]
-  `(locking ~playlist
-     (let [~'it ~playlist]
-       ~@body)))
+     (reduce #(when (.add ^java.util.List %1 %2) %1)
+             (->ConcurrentArrayList) sources)))
 
 (defn append-source!
   "Append a source to a playlist. A source is anything that
   clojure.java.io/input-stream can take. Returns the modified
   playlist."
-  [playlist source]
-  (with-playlist playlist
-    (when (.add it source) it)))
+  [^java.util.List playlist source]
+  (locking playlist
+    (when (.add playlist source) playlist)))
 
 (defn insert-source!
   "Insert a source in to a playlist. A source is anything that
   clojure.java.io/input-stream can take. If idx is greater than the
   length of the playlist, the source will be appended. Returns the
   modified playlist."
-  [playlist source idx]
+  [^java.util.List playlist source idx]
   {:pre [(>= idx 0)]}
-  (with-playlist playlist
-    (if (> idx (count it))
-      (append-source! it source)
-      (do (.add it idx source) it))))
+  (locking playlist
+    (if (> idx (count playlist))
+      (append-source! playlist source)
+      (do (.add playlist idx source) playlist))))
 
 (defn remove-source!
   "Remove a source from the playlist. This is a noop if idx isn't in
   the playlist. Returns the modified playlist."
-  [playlist idx]
+  [^java.util.List playlist idx]
   {:pre [(>= idx 0)]}
-  (with-playlist playlist
-    (when (< idx (count it))
-      (.remove #^java.util.List it (int idx)))
-    it))
+  (locking playlist
+    (when (< idx (count playlist))
+      (.remove #^java.util.List playlist (int idx)))
+    playlist))
 
 (defn move-source!
   "Move a source in the playlist from one position to another. This is
@@ -60,11 +107,11 @@
   before the source is moved. This operation is atomic."
   [playlist from-idx to-idx]
   {:pre [(>= from-idx 0) (>= to-idx 0)]}
-  (with-playlist playlist
-    (if (>= from-idx (count it))
-      it
-      (let [source (nth it from-idx)]
-        (insert-source! it source to-idx)
-        (remove-source! it (if (< to-idx from-idx)
+  (locking playlist
+    (if (>= from-idx (count playlist))
+      playlist
+      (let [source (nth playlist from-idx)]
+        (insert-source! playlist source to-idx)
+        (remove-source! playlist (if (< to-idx from-idx)
                                    (inc from-idx)
                                    from-idx))))))
